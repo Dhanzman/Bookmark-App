2cd3322d28e29ea46b3ca3d7875c755bea2979c7
Dhanzman <danielsosimi123@yahoo.com>
2022-04-15 20:14:46 +0000

    Bookmark Function

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const modalClose = document.getElementById('close-modal');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
-const bookmarksContainer = document.getElementById('booksmarks-container');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+let bookmarks = [];
 
 // Show Modal, Focus on input
 function showModal() {
@@ -38,6 +40,64 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+// Build Bookmarks DOM
+function buildBookmarks() {
+  // Build Items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // Item
+    const item = document.createElement('div');
+    item.classList.add('item');
+    // Close Icon
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fas', 'fa-times');
+    closeIcon.setAttribute('title', 'Delete Bookmark');
+    closeIcon.setAttribute('onclick', `deleteBookmark(${url})`);
+    // Favicon / Link container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+    // Favicon
+    const favicon = document.createElement('img');
+    favicon.setAttribute(
+      'src',
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`,
+    );
+    favicon.setAttribute('alt', 'Favicon');
+    // Link
+    const linkEl = document.createElement('a');
+    linkEl.setAttribute('href', `${url}`);
+    linkEl.setAttribute('target', '_blank');
+    linkEl.textContent = name;
+    // Append to Bookmark container
+    linkInfo.append(favicon, linkEl);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
+// Fetch Bookmarks
+function fetchBookmarks() {
+  // Get Bookmark from localstorage if available
+  if (localStorage.getItem('bookmarks')) {
+    bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  } else {
+    //Craete bookmarks array in local storage
+    bookmarks = [
+      {
+        name: 'Google',
+        url: 'https://google.com',
+      },
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+  console.log(url);
+}
+
 // Handle Data from Form
 function storeBookmark(e) {
   e.preventDefault();
@@ -49,7 +109,19 @@ function storeBookmark(e) {
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 }
 
 // Event listener
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+// On Load , Fetch Bookmarks
+fetchBookmarks();
